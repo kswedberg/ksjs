e6060cd8241df74a95bcbb17e553be1a0346cbb2
Karl Swedberg <kswedberg@gmail.com>
2021-11-09 15:59:12 -0500

    Update docs building

--- scripts/build-docs.js
+++ scripts/build-docs.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const fs = require('fs-extra');
 const jsdoc2md = require('jsdoc-to-markdown');
+const Markdown = require('markdown-it');
+
 const {cleanMarkdown} = require('./clean-markdown.js');
 const projRoot = path.join(__dirname, '..');
 
@@ -14,17 +16,61 @@ const renderAPI = async(files) => {
 
   return output;
 };
-
-const outputAPI = async(files) => {
+const buildMarkdown = async(files) => {
+  let content = '';
   try {
     const output = await renderAPI(files);
     const intro = await fs.readFile(path.join(__dirname, 'partials', 'intro.md'));
-    const apiMd = path.join(__dirname, 'api.md');
-    const content = [intro, output].join('\n');
 
-    await fs.outputFile(apiMd, content);
+    content = [intro, output].join('\n');
+  } catch (err) {
+    console.log(err);
+  }
 
-    cleanMarkdown(apiMd, 'Finished building README.md!!');
+  return content;
+};
+
+const md2html = (content) => {
+  const md = Markdown({
+    html: true,
+    typographer: true,
+  });
+
+  return md.render(content);
+};
+
+const outputAPI = async(files) => {
+  const content = await buildMarkdown(files);
+  try {
+    const apiMdFile = path.join(__dirname, 'api.md');
+
+    await fs.outputFile(apiMdFile, content);
+
+    const md = await cleanMarkdown(apiMdFile);
+
+    console.log('Finished building README.md!!');
+    const html = md2html(md);
+    const readmeHtml = `
+<html>
+  <head>
+  <title>BAMF JS</title>
+  <style>
+    html, body {
+      font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;
+    }
+    body,
+    input,
+    textarea,
+    select {
+      font-size: 100%;
+    }
+  </style>
+  </head>
+  <body>
+    ${html}
+  </body>
+</html>`
+    await fs.outputFile(path.join(projRoot, 'README.html'), readmeHtml);
   } catch (err) {
     console.log(err);
   }
--- scripts/clean-markdown.js
+++ scripts/clean-markdown.js
@@ -2,30 +2,34 @@ const path = require('path');
 const through2 = require('through2');
 const fs = require('fs-extra');
 const projRoot = path.join(__dirname, '../');
+const readmeMd = path.join(projRoot, 'README.md');
 
-const cleanMarkdown = (file, completeMsg) => {
-  const rDollar = /\[\$/g;
-  const inputFile = file || path.join(projRoot, 'scripts/README.md');
+const cleanMarkdown = (file, onFinish) => {
+  return new Promise((resolve, reject) => {
+    const rDollar = /\[\$/g;
+      const inputFile = file || path.join(projRoot, 'scripts/README.md');
 
-  fs.createReadStream(inputFile)
-  .pipe(through2(function(buff, enc, callback) {
-    let chunk = buff.toString();
+      fs.createReadStream(inputFile)
+      .pipe(through2(function(buff, enc, callback) {
+        let chunk = buff.toString();
 
-    if (rDollar.test(chunk)) {
-      chunk = chunk.replace(rDollar, '[\\$');
-    }
-    if (chunk.includes('**Properties**')) {
-      chunk = chunk.replace('**Properties**', '**Methods**');
-    }
-    chunk = chunk.replace(/&quot;(location.href)&quot;/g, '$1');
-    this.push(chunk);
+        if (rDollar.test(chunk)) {
+          chunk = chunk.replace(rDollar, '[\\$');
+        }
+        if (chunk.includes('**Properties**')) {
+          chunk = chunk.replace('**Properties**', '**Methods**');
+        }
+        chunk = chunk.replace(/&quot;(location.href)&quot;/g, '$1');
+        this.push(chunk);
 
-    callback();
-  }))
-  .pipe(fs.createWriteStream(path.join(projRoot, 'README.md'))
-  .on('finish', () => {
-    console.log(completeMsg || 'Finished cleaning README.md!');
-  }));
+        callback();
+      }))
+      .pipe(fs.createWriteStream(readmeMd)
+      .on('finish', () => {
+        return fs.readFile(readmeMd, 'utf8')
+        .then(resolve);
+      }));
+  });
 };
 
 // @ts-ignore
